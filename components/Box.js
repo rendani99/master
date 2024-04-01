ce2d156e07b6aaa5611ddd41687d4031b870da6c
rendani99 <rendania460@gmail.com>
2024-04-01 05:20:56 +0200

    set

--- components/Box.js
+++ components/Box.js
@@ -4,8 +4,13 @@ import { View, Text } from 'react-native';
 export default function componentName() {
   return (
     <View>
-      <Text></Text>
+      <Text>Box</Text>
      </View>
   );
 }
 
+const styles = StyleSheet.create({
+  box:{backgroundColor:"#fff",
+padding},
+
+})
